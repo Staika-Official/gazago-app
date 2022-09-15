4b2bab8344475f3cb79fe6e9005e17e97ef79151
Juan-EZ <iwpark@eztechfin.co.kr>
2022-09-15 19:32:31 +0900

    home controller원복

--- lib/presentations/views/home.dart
+++ lib/presentations/views/home.dart
@@ -1,29 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:step_go/platform/controllers/home_menu_controller.dart';
-import 'package:step_go/presentations/components/main_appbar.dart';
-import 'package:step_go/presentations/components/secondary_appbar.dart';
-import 'package:step_go/presentations/views/activity/index.dart';
-import 'package:step_go/presentations/views/archive/index.dart';
-import 'package:step_go/presentations/views/inventory/index.dart';
-import 'package:step_go/presentations/views/leaderboard/index.dart';
-import 'package:step_go/presentations/views/shop/index.dart';
 
 class Home extends StatelessWidget {
-  Home({Key? key}) : super(key: key);
-
-  final List<PreferredSizeWidget> appbarList = [
-    MainAppbar(),
-    SecondaryAppbar(),
-  ];
-
-  final List<Widget> mainViewWidgetList = [
-    ArchiveHome(),
-    InventoryHome(),
-    ActivityHome(),
-    ShopHome(),
-    LeaderboardHome(),
-  ];
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +11,8 @@ class Home extends StatelessWidget {
 
     return Obx(() {
       return Scaffold(
-        appBar: controller.getAppbar(appbarList),
-        body: mainViewWidgetList.elementAt(controller.selectedIndex.value),
+        appBar: controller.appbar,
+        body: controller.mainViewWidgetList.elementAt(controller.selectedIndex.value),
         bottomNavigationBar: NavigationBar(
           onDestinationSelected: (index) => controller.selectMenu(index),
           selectedIndex: controller.selectedIndex.value,
