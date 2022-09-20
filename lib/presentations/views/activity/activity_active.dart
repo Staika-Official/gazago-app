1088ac05db2611086f696707af81b13f29993350
Juan-EZ <iwpark@eztechfin.co.kr>
2022-09-20 18:43:33 +0900

    앱 위치 권한 및 지도 추가

---
+++ lib/presentations/views/activity/activity_active.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:gaza_go/platform/controllers/activity_active_controller.dart';
+import 'package:gaza_go/presentations/components/default_container.dart';
+import 'package:get/get.dart';
+
+class ActivityActive extends StatelessWidget {
+  const ActivityActive({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ActivityActiveController controller = Get.put(ActivityActiveController());
+
+    return DefaultContainer(
+      child: Center(
+        child: Obx(() {
+          return Column(
+            children: [
+              Expanded(
+                child: NaverMap(
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(controller.currentLocation.value.latitude ?? 0, controller.currentLocation.value.longitude ?? 0),
+                    zoom: 10,
+                  ),
+                  initLocationTrackingMode: LocationTrackingMode.Follow,
+                  mapType: MapType.Basic,
+                  pathOverlays: (controller.coordinates.length < 10)
+                      ? null
+                      : {
+                          PathOverlay(
+                            PathOverlayId('path'),
+                            controller.coordinates,
+                            width: 3,
+                            color: Colors.red,
+                            outlineColor: Colors.white,
+                          )
+                        },
+                  locationButtonEnable: true,
+                  onCameraChange: controller.onCameraChange,
+                  onCameraIdle: controller.onCameraIdle,
+                  onMapCreated: controller.onMapCreated,
+                  onMapTap: controller.onMapTap,
+                  onMapLongTap: controller.onMapLongTap,
+                  onMapDoubleTap: controller.onMapDoubleTap,
+                  onMapTwoFingerTap: controller.onMapTwoFingerTap,
+                  onSymbolTap: controller.onSymbolTap,
+                  indoorEnable: true,
+                  maxZoom: 17,
+                  minZoom: 15,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(controller.currentLocation.value.latitude.toString()),
+                  Text(controller.currentLocation.value.longitude.toString()),
+                ],
+              )
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
