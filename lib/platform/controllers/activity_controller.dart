1c73b3a841351481ce55e7a30556e5fbdd1b48f8
Juan-EZ <iwpark@eztechfin.co.kr>
2022-10-20 17:08:27 +0900

    멈춤이 아닐경우에만 운동 데이터 쌓이도록 수정

--- lib/platform/controllers/activity_controller.dart
+++ lib/platform/controllers/activity_controller.dart
@@ -666,7 +666,7 @@ class ActivityController extends GetxController with ActivityMixin, ChallengeMix
     locationSubscription ??= Geolocator.getPositionStream(locationSettings: locationSettings).listen((Position position) {
       currentLocation.value = position;
 
-      if (exerciseState.value == ExerciseState.ongoing) {
+      if (exerciseState.value == ExerciseState.ongoing && pedestrianStatus.value != 'STOPPED') {
         exerciseData.add(UserExerciseModel(
           altitude: position.altitude,
           speed: position.speed,
