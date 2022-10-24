ab57365ec232a18b6abafac6751a1cacf883fde5
kjh4357 <kjh4357@gmail.com>
2022-10-24 19:04:23 +0900

    장착한 아이템 스크롤 제거

--- lib/presentations/views/activity/equipped_item.dart
+++ lib/presentations/views/activity/equipped_item.dart
@@ -25,180 +25,139 @@ class EquippedItems extends StatelessWidget {
             alignment: Alignment(0, 1),
           ),
         ),
-        child: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-                  child: Column(
-                    children: [
-                      Obx(() {
-                        return StaggeredGrid.count(
-                          crossAxisCount: 4,
-                          mainAxisSpacing: 6,
-                          crossAxisSpacing: 6,
-                          children: [
-                            if (controller.equippedItemList.isNotEmpty) ...[
-                              StaggeredGridTile.count(
-                                crossAxisCellCount: 2,
-                                mainAxisCellCount: 2,
-                                child: Tile(
-                                  index: 0,
-                                  id: controller.equippedShoe.value.id,
-                                  itemGrade: controller.equippedShoe.value.itemGrade,
-                                  durability: controller.equippedShoe.value.durability,
-                                  imageUrl: controller.equippedShoe.value.itemImageUrl,
-                                ),
-                              ),
-                              StaggeredGridTile.count(
-                                crossAxisCellCount: 2,
-                                mainAxisCellCount: 2,
-                                child: Tile(
-                                  index: 1,
-                                  imageUrl: controller.equippedBadge.value.badge.imageUrl,
-                                ),
-                              ),
-                              StaggeredGridTile.count(
-                                crossAxisCellCount: 1,
-                                mainAxisCellCount: 1,
-                                child: Tile(
-                                  index: 2,
-                                  itemGrade: controller.equippedHat.value.itemGrade,
-                                  imageUrl: controller.equippedHat.value.itemImageUrl,
-                                ),
-                              ),
-                              StaggeredGridTile.count(
-                                crossAxisCellCount: 1,
-                                mainAxisCellCount: 1,
-                                child: Tile(
-                                  index: 3,
-                                  itemGrade: controller.equippedTop.value.itemGrade,
-                                  imageUrl: controller.equippedTop.value.itemImageUrl,
-                                ),
-                              ),
-                              StaggeredGridTile.count(
-                                crossAxisCellCount: 1,
-                                mainAxisCellCount: 1,
-                                child: Tile(
-                                  index: 4,
-                                  itemGrade: controller.equippedBottom.value.itemGrade,
-                                  imageUrl: controller.equippedBottom.value.itemImageUrl,
-                                ),
-                              ),
-                              StaggeredGridTile.count(
-                                crossAxisCellCount: 1,
-                                mainAxisCellCount: 1,
-                                child: Tile(
-                                  index: 5,
-                                  itemGrade: controller.equippedAccessory.value.itemGrade,
-                                  imageUrl: controller.equippedAccessory.value.itemImageUrl,
-                                ),
-                              ),
-                            ]
-                          ],
-                        );
-                      }),
-                    ],
-                  ),
-                ),
-                Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 20.0),
-                    child: Obx(() {
-                      return Row(
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                child: Column(
+                  children: [
+                    Obx(() {
+                      return StaggeredGrid.count(
+                        crossAxisCount: 4,
+                        mainAxisSpacing: 6,
+                        crossAxisSpacing: 6,
                         children: [
-                          Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  StyledText(
-                                    '${controller.equippedRewardRate.toInt()}',
-                                    fontSize: 28,
-                                    fontWeight: 500,
-                                  ),
-                                  StyledText(
-                                    '%',
-                                    fontSize: 16,
-                                    fontWeight: 500,
-                                  ),
-                                ],
+                          if (controller.equippedItemList.isNotEmpty) ...[
+                            StaggeredGridTile.count(
+                              crossAxisCellCount: 2,
+                              mainAxisCellCount: 2,
+                              child: Tile(
+                                index: 0,
+                                id: controller.equippedShoe.value.id,
+                                itemGrade: controller.equippedShoe.value.itemGrade,
+                                durability: controller.equippedShoe.value.durability,
+                                imageUrl: controller.equippedShoe.value.itemImageUrl,
                               ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 12.0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 3.0, right: 2.0),
-                                      child: iconGoReward,
-                                    ),
-                                    StyledText(
-                                      'GO 보상율',
-                                      color: Color(0xFF8A8A8A),
-                                      fontSize: 11,
-                                      lineHeight: 12,
-                                      fontWeight: 500,
-                                    ),
-                                  ],
-                                ),
+                            ),
+                            StaggeredGridTile.count(
+                              crossAxisCellCount: 2,
+                              mainAxisCellCount: 2,
+                              child: Tile(
+                                index: 1,
+                                imageUrl: controller.equippedBadge.value.badge.imageUrl,
                               ),
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                            child: Column(
+                            ),
+                            StaggeredGridTile.count(
+                              crossAxisCellCount: 1,
+                              mainAxisCellCount: 1,
+                              child: Tile(
+                                index: 2,
+                                itemGrade: controller.equippedHat.value.itemGrade,
+                                imageUrl: controller.equippedHat.value.itemImageUrl,
+                              ),
+                            ),
+                            StaggeredGridTile.count(
+                              crossAxisCellCount: 1,
+                              mainAxisCellCount: 1,
+                              child: Tile(
+                                index: 3,
+                                itemGrade: controller.equippedTop.value.itemGrade,
+                                imageUrl: controller.equippedTop.value.itemImageUrl,
+                              ),
+                            ),
+                            StaggeredGridTile.count(
+                              crossAxisCellCount: 1,
+                              mainAxisCellCount: 1,
+                              child: Tile(
+                                index: 4,
+                                itemGrade: controller.equippedBottom.value.itemGrade,
+                                imageUrl: controller.equippedBottom.value.itemImageUrl,
+                              ),
+                            ),
+                            StaggeredGridTile.count(
+                              crossAxisCellCount: 1,
+                              mainAxisCellCount: 1,
+                              child: Tile(
+                                index: 5,
+                                itemGrade: controller.equippedAccessory.value.itemGrade,
+                                imageUrl: controller.equippedAccessory.value.itemImageUrl,
+                              ),
+                            ),
+                          ]
+                        ],
+                      );
+                    }),
+                  ],
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Obx(() {
+                    return Row(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    StyledText(
-                                      '${controller.equippedAbrasionRate.toInt()}',
-                                      fontSize: 28,
-                                      fontWeight: 500,
-                                    ),
-                                    StyledText(
-                                      '%',
-                                      fontSize: 16,
-                                      fontWeight: 500,
-                                    ),
-                                  ],
+                                StyledText(
+                                  '${controller.equippedRewardRate.toInt()}',
+                                  fontSize: 28,
+                                  fontWeight: 500,
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 12.0),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(top: 1.0, right: 3.0),
-                                        child: iconItemAbrasion,
-                                      ),
-                                      StyledText(
-                                        '아이템 마모율',
-                                        color: Color(0xFF8A8A8A),
-                                        fontSize: 11,
-                                        lineHeight: 12,
-                                        fontWeight: 500,
-                                      ),
-                                    ],
-                                  ),
+                                StyledText(
+                                  '%',
+                                  fontSize: 16,
+                                  fontWeight: 500,
                                 ),
                               ],
                             ),
-                          ),
-                          Column(
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 3.0, right: 2.0),
+                                    child: iconGoReward,
+                                  ),
+                                  StyledText(
+                                    'GO 보상율',
+                                    color: Color(0xFF8A8A8A),
+                                    fontSize: 11,
+                                    lineHeight: 12,
+                                    fontWeight: 500,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                          child: Column(
                             children: [
                               Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
                                   StyledText(
-                                    '${controller.equippedStaminaReduceRate.toInt()}',
+                                    '${controller.equippedAbrasionRate.toInt()}',
                                     fontSize: 28,
                                     fontWeight: 500,
                                   ),
@@ -213,13 +172,14 @@ class EquippedItems extends StatelessWidget {
                                 padding: const EdgeInsets.only(top: 12.0),
                                 child: Row(
                                   mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
                                     Padding(
                                       padding: const EdgeInsets.only(top: 1.0, right: 3.0),
-                                      child: iconStaminaReduce,
+                                      child: iconItemAbrasion,
                                     ),
                                     StyledText(
-                                      '체력 감소율',
+                                      '아이템 마모율',
                                       color: Color(0xFF8A8A8A),
                                       fontSize: 11,
                                       lineHeight: 12,
@@ -230,11 +190,49 @@ class EquippedItems extends StatelessWidget {
                               ),
                             ],
                           ),
-                        ],
-                      );
-                    })),
-              ],
-            ),
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                StyledText(
+                                  '${controller.equippedStaminaReduceRate.toInt()}',
+                                  fontSize: 28,
+                                  fontWeight: 500,
+                                ),
+                                StyledText(
+                                  '%',
+                                  fontSize: 16,
+                                  fontWeight: 500,
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 1.0, right: 3.0),
+                                    child: iconStaminaReduce,
+                                  ),
+                                  StyledText(
+                                    '체력 감소율',
+                                    color: Color(0xFF8A8A8A),
+                                    fontSize: 11,
+                                    lineHeight: 12,
+                                    fontWeight: 500,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  })),
+            ],
           ),
         ),
       ),
