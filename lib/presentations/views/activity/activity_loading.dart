9d8fc240db4c7e96b39a93ef8d465b36a2f76aca
Scott <coolrainy24@gmail.com>
2022-11-03 17:05:31 +0900

    1. 가자고 애니메이션 2. 아이템 스크롤

--- lib/presentations/views/activity/activity_loading.dart
+++ lib/presentations/views/activity/activity_loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gaza_go/platform/controllers/activity_controller.dart';
 import 'package:get/get.dart';
+import 'package:simple_animations/animation_builder/loop_animation_builder.dart';
 
 class ActivityLoading extends StatelessWidget {
   const ActivityLoading({Key? key}) : super(key: key);
@@ -11,10 +12,22 @@ class ActivityLoading extends StatelessWidget {
 
     return Obx(() {
       return Center(
-        child: Padding(
+        child: LoopAnimationBuilder<double>(
+          tween: Tween(begin: 0.1, end: 1.1),
+          duration: const Duration(seconds: 1),
+          curve: Curves.easeOut,
+          builder: (context, value, child) {
+            return Transform.scale(
+              scale: value,
+              child: child,
+            );
+          },
+          child: Image.asset('assets/images/activity/ico_loading_${controller.loadingTime.value}.png'),
+        )
+        /*child: Padding(
           padding: const EdgeInsets.all(20),
           child: Image.asset('assets/images/activity/ico_loading_${controller.loadingTime.value}.png'),
-        ),
+        ),*/
       );
     });
   }
