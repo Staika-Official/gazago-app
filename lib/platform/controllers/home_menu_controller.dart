f05f9b11daaa3fde550b11d09a8fc06d2a85ce40
Juan-EZ <iwpark@eztechfin.co.kr>
2022-11-24 17:06:02 +0900

    보상 푸시가 백그라운드 또는 종료상태에서 왔을 경우 랭킹화면으로 이동하도록 처리

--- lib/platform/controllers/home_menu_controller.dart
+++ lib/platform/controllers/home_menu_controller.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:gaza_go/constants/enums.dart';
 import 'package:gaza_go/constants/routes.dart';
 import 'package:gaza_go/platform/controllers/activity_controller.dart';
 import 'package:gaza_go/platform/controllers/archive_controller.dart';
 import 'package:gaza_go/platform/controllers/inventory_controller.dart';
 import 'package:gaza_go/platform/controllers/leaderboard_controller.dart';
+import 'package:gaza_go/platform/stores/hive_store.dart';
 import 'package:gaza_go/presentations/components/main_appbar.dart';
 import 'package:gaza_go/presentations/components/secondary_appbar.dart';
 import 'package:gaza_go/presentations/views/activity/index.dart';
@@ -12,7 +15,7 @@ import 'package:gaza_go/presentations/views/inventory/index.dart';
 import 'package:gaza_go/presentations/views/leaderboard/index.dart';
 import 'package:get/get.dart';
 
-class HomeMenuController extends GetxController {
+class HomeMenuController extends SuperController {
   final RxInt selectedIndex = RxInt(0);
   final RxInt prevIndex = RxInt(0);
   final RxList<int> visitedTabs = RxList.empty();
@@ -36,6 +39,22 @@ class HomeMenuController extends GetxController {
         return appbarList.first;
       default:
         return appbarList.last;
+    }
+  }
+
+  @override
+  void onReady() {
+    handleRewardNotification();
+    super.onReady();
+  }
+
+  void handleRewardNotification() async {
+    RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+
+    if (initialMessage != null) {
+      if (initialMessage.data['notificationKey'] == 'DAILY_REWARD_COMPLETED') {
+        selectMenu(3);
+      }
     }
   }
 
@@ -68,6 +87,29 @@ class HomeMenuController extends GetxController {
       }
     } else {
       visitedTabs.add(index);
+    }
+  }
+
+  @override
+  void onDetached() {
+    // TODO: implement onDetached
+  }
+
+  @override
+  void onInactive() {
+    // TODO: implement onInactive
+  }
+
+  @override
+  void onPaused() {
+    // TODO: implement onPaused
+  }
+
+  @override
+  void onResumed() {
+    if (HiveStore.load(key: HiveKey.needRouteToRanking.name) != null && HiveStore.load(key: HiveKey.needRouteToRanking.name)) {
+      HiveStore.deleteKey(key: HiveKey.needRouteToRanking.name);
+      selectMenu(3);
     }
   }
 }
