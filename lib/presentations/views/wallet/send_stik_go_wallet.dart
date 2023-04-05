743424f3beddc788515e7e48bafd62906ae19113
kjh4357 <kjh4357@gmail.com>
2023-04-05 16:51:21 +0900

    스틱 보내기 화면 작업중

--- lib/presentations/views/wallet/send_stik_go_wallet.dart
+++ lib/presentations/views/wallet/send_stik_go_wallet.dart
@@ -21,117 +21,145 @@ class SendStikGoWallet extends StatelessWidget {
         titleText: 'GO 지갑으로 보내기',
         backgroundColor: subBg01Color,
         headerBackgroundColor: Colors.transparent,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 28.0.sp, left: 22.sp, right: 22.sp),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    radius: 19.sp,
-                    foregroundImage: controller.assetStik.value != null
-                        ? CachedNetworkImageProvider(
-                            controller.assetStik.value.logoUrl!,
-                          )
-                        : Image.asset(
-                            'assets/images/ic_launcher.png',
-                            width: 23.sp,
-                          ).image,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.0.sp),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        StyledText(
-                          '보유 중',
-                          color: lightGrayColor,
-                          fontSize: 12,
-                          lineHeight: 13,
-                          fontWeight: 600,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 6.0.sp),
-                          child: StyledText(
-                            '${controller.assetStik.value.uiAmount} STIK',
-                            fontSize: 18,
-                            lineHeight: 19,
-                            fontWeight: 500,
-                          ),
-                        ),
-                      ],
+        child: Obx(() {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 28.0.sp, left: 22.sp, right: 22.sp),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 19.sp,
+                      foregroundImage: controller.assetStik.value != null
+                          ? CachedNetworkImageProvider(
+                              controller.assetStik.value.logoUrl!,
+                            )
+                          : Image.asset(
+                              'assets/images/ic_launcher.png',
+                              width: 23.sp,
+                            ).image,
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 50.0.sp, left: 28.sp, right: 28.sp),
-              child: StyledText(
-                '보내는 STIK',
-                fontWeight: 500,
-                fontSize: 18,
-                lineHeight: 20,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 12.0.sp, left: 28.sp, right: 28.sp),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: '금액을 입력하세요(STIK)',
-                      hintStyle: TextStyle(
-                        color: deepGrayColor,
-                        fontWeight: FontWeight.w400,
+                    Padding(
+                      padding: EdgeInsets.only(left: 10.0.sp),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          StyledText(
+                            '보유 중',
+                            color: lightGrayColor,
+                            fontSize: 12,
+                            lineHeight: 13,
+                            fontWeight: 600,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 6.0.sp),
+                            child: StyledText(
+                              '${controller.assetStik.value.uiAmount} STIK',
+                              fontSize: 18,
+                              lineHeight: 19,
+                              fontWeight: 500,
+                            ),
+                          ),
+                        ],
                       ),
-                      suffixText: ' STIK',
-                      suffixStyle: TextStyle(
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 50.0.sp, left: 28.sp, right: 28.sp),
+                child: StyledText(
+                  '보내는 STIK',
+                  fontWeight: 500,
+                  fontSize: 18,
+                  lineHeight: 20,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 12.0.sp, left: 28.sp, right: 28.sp),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: '금액을 입력하세요(STIK)',
+                        hintStyle: TextStyle(
+                          color: deepGrayColor,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        suffixText: controller.focusNode != null ? ' STIK' : '',
+                        suffixStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(width: 2, color: Color(0xFF363841)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(width: 2, color: Color(0xFF363841)),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                      ),
+                      controller: controller.stikAmountTextController,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
                         color: Colors.white,
                         fontSize: 24.sp,
-                        fontWeight: FontWeight.w400,
+                        height: 28 / 24,
+                        fontWeight: FontWeight.w600,
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(width: 1, color: Color(0xFF363841)),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(width: 1, color: Color(0xFF363841)),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      autofocus: false,
+                      cursorColor: Colors.white,
+                      focusNode: controller.focusNode,
+                      onChanged: (value) => controller.setAmount(value),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 15.0.sp),
+                      child: StyledText(
+                        '* 보내는 STIK의 0.5%가 전송수수료로 추가 사용됩니다.',
+                        fontSize: 12,
+                        lineHeight: 20,
+                        color: deepGrayColor,
                       ),
                     ),
-                    controller: controller.stikAmountTextController,
-                    keyboardType: TextInputType.number,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.sp,
-                      height: 28 / 24,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    autofocus: false,
-                    cursorColor: Colors.white,
-                    focusNode: controller.focusNode,
-                    onChanged: (value) => controller.setAmount(value),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 15.0.sp),
-                    child: StyledText(
-                      '* 보내는 STIK의 0.5%가 전송수수료로 추가 사용됩니다.',
-                      fontSize: 12,
-                      lineHeight: 20,
-                      color: deepGrayColor,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
+              Expanded(
+                child: Container(
+                    color: subBg01Color,
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        color: controller.sendStikAmount == 0.0 ? popupBgColor : skyBlueColor,
+                        height: 60.sp,
+                        alignment: Alignment.center,
+                        child: InkWell(
+                          onTap: () => null,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8.0.sp),
+                            child: Center(
+                              child: StyledText(
+                                '보내기',
+                                color: controller.sendStikAmount == 0.0 ? deepGrayColor : Colors.black,
+                                fontSize: 18,
+                                fontWeight: 500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
+              )
+            ],
+          );
+        }),
       ),
     );
   }
