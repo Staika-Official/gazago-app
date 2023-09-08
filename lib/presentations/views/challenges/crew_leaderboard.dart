17c449e379de73f4f6176a007b3d390c17311d40
Juan-EZ <iwpark@eztechfin.co.kr>
2023-09-08 11:08:44 +0900

    랭킹에서 내 크루 선택 시 상세로 이동되도록 수정

--- lib/presentations/views/challenges/crew_leaderboard.dart
+++ lib/presentations/views/challenges/crew_leaderboard.dart
@@ -53,99 +53,107 @@ class CrewRankingItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: isMyCrew ? Colors.black : subBg01Color,
-      height: 64.sp,
-      padding: EdgeInsets.only(left: 18.sp, right: 20.sp),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Expanded(
-            child: SizedBox(
-              height: double.infinity,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ConstrainedBox(
-                    constraints: const BoxConstraints(maxWidth: 50, minWidth: 40),
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: StyledText(
-                        (index + 1).toString(),
-                        fontWeight: 600,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10.0.sp),
-                    child: Center(
-                      child: CircleAvatar(
-                        radius: 22.sp,
-                        backgroundColor: deepGrayColor,
-                        foregroundImage: (item.iconImageUrl == null || item.iconImageUrl == '')
-                            ? Image.asset(
-                                'assets/images/ic_launcher.png',
-                                width: 44.sp,
-                              ).image
-                            : item.iconImageUrl!.contains('.svg')
-                                ? sp.Svg(item.iconImageUrl!, source: sp.SvgSource.network) as ImageProvider
-                                : CachedNetworkImageProvider(
-                                    item.iconImageUrl!,
-                                    headers: imageNetworkHeader,
-                                  ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20.sp),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          FittedBox(
+    return Material(
+      color: Colors.transparent,
+      child: Ink(
+        color: isMyCrew ? Colors.black : subBg01Color,
+        height: 64.sp,
+        child: InkWell(
+          onTap: isMyCrew ? () => Get.find<ChallengesDetailController>().moveToMyCrew() : null,
+          child: Padding(
+            padding: EdgeInsets.only(left: 18.sp, right: 20.sp),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    height: double.infinity,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ConstrainedBox(
+                          constraints: const BoxConstraints(maxWidth: 50, minWidth: 40),
+                          child: FittedBox(
                             fit: BoxFit.scaleDown,
                             child: StyledText(
-                              item.name!,
-                              fontWeight: 500,
+                              (index + 1).toString(),
+                              fontWeight: 600,
                               fontSize: 16,
-                              lineHeight: 20,
-                              letterSpacing: 0,
-                              overflowEllipsis: true,
                             ),
                           ),
-                          StyledText(
-                            '크루장 : ${item.crewFounderNickName!.split('@')[0]}',
-                            color: deepGrayColor,
-                            fontWeight: 500,
-                            fontSize: 12,
-                            lineHeight: 16,
-                            letterSpacing: 0,
-                            overflowEllipsis: true,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 10.0.sp),
+                          child: Center(
+                            child: CircleAvatar(
+                              radius: 22.sp,
+                              backgroundColor: deepGrayColor,
+                              foregroundImage: (item.iconImageUrl == null || item.iconImageUrl == '')
+                                  ? Image.asset(
+                                      'assets/images/ic_launcher.png',
+                                      width: 44.sp,
+                                    ).image
+                                  : item.iconImageUrl!.contains('.svg')
+                                      ? sp.Svg(item.iconImageUrl!, source: sp.SvgSource.network) as ImageProvider
+                                      : CachedNetworkImageProvider(
+                                          item.iconImageUrl!,
+                                          headers: imageNetworkHeader,
+                                        ),
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 20.sp),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                FittedBox(
+                                  fit: BoxFit.scaleDown,
+                                  child: StyledText(
+                                    item.name!,
+                                    fontWeight: 500,
+                                    fontSize: 16,
+                                    lineHeight: 20,
+                                    letterSpacing: 0,
+                                    overflowEllipsis: true,
+                                  ),
+                                ),
+                                StyledText(
+                                  '크루장 : ${item.crewFounderNickName!.split('@')[0]}',
+                                  color: deepGrayColor,
+                                  fontWeight: 500,
+                                  fontSize: 12,
+                                  lineHeight: 16,
+                                  letterSpacing: 0,
+                                  overflowEllipsis: true,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: StyledText(
+                    '${item.blockQuantity} 블럭',
+                    textAlign: TextAlign.right,
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: 700,
+                    softWrap: false,
+                  ),
+                ),
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8),
-            child: StyledText(
-              '${item.blockQuantity} 블럭',
-              textAlign: TextAlign.right,
-              color: Colors.white,
-              fontSize: 14,
-              fontWeight: 700,
-              softWrap: false,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
