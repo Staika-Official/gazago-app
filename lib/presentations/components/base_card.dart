a7b991ddd0fc8bf9186f12f09678f8285bc993b4
khoi.tran <khoi.tran@sotatek.com>
2025-08-20 14:34:07 +0700

    Fork from dev for treasure feature

---
+++ lib/presentations/components/base_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class BaseCard extends StatelessWidget {
+  const BaseCard({
+    super.key,
+    this.child,
+    this.backgroundColor = const Color(0xff222222),
+    this.borderRadius = 0,
+    this.padding,
+  });
+
+  final Widget? child;
+  final Color backgroundColor;
+  final double borderRadius;
+  final EdgeInsets? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius ?? 0),
+        color: backgroundColor,
+      ),
+      child: child,
+    );
+  }
+}
