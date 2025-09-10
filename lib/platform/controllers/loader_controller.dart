d3b62d89d249f5d7838870bb16f9771443e180f9
khoi.tran <khoi.tran@sotatek.com>
2025-09-10 11:35:44 +0700

    update: add loading when load treasure, revert loader controller

--- lib/platform/controllers/loader_controller.dart
+++ lib/platform/controllers/loader_controller.dart
@@ -4,37 +4,45 @@ import 'package:gaza_go/theme/theme.g.dart';
 import 'package:get/get.dart';
 
 class LoaderController extends GetxController {
+  GlobalKey<State<StatefulWidget>> dialogKey = GlobalKey();
   RxBool isLoading = RxBool(false);
+  GlobalKey? loaderKey = GlobalKey();
 
   @override
   Future<void> onInit() async {
-    isLoading.listen((isTrue) {
-      if (isTrue) {
+    isLoading.listen((val) {
+      if (val == true) {
         Get.dialog(
-          barrierDismissible: false,
-          Dialog(
-            shadowColor: Colors.transparent,
-            backgroundColor: Colors.transparent,
-            child: Center(
-              child: SizedBox.square(
-                dimension: 32,
-                child: CircularProgressIndicator(
-                  color:
-                      AppColorData.regular().colorIconInteractivePrimaryPressed,
-                  backgroundColor: AppColorData.regular()
-                      .colorIconInteractivePrimaryPressed
-                      .withOpacity(0.2),
+            barrierDismissible: false,
+            Dialog(
+              shadowColor: Colors.transparent,
+              backgroundColor: Colors.transparent,
+              child: Center(
+                child: SizedBox.square(
+                  dimension: 32,
+                  child: CircularProgressIndicator(
+                    color: AppColorData.regular()
+                        .colorIconInteractivePrimaryPressed,
+                    backgroundColor: AppColorData.regular()
+                        .colorIconInteractivePrimaryPressed
+                        .withOpacity(0.2),
+                  ),
                 ),
               ),
             ),
-          ),
-          name: 'progressCircle',
-        );
+            name: 'progressCircle');
+        // if (!Get.isDialogOpen!) {
+        //   Get.dialog(Loader(), name: 'progressCircle');
+        // }
       } else {
-        if (Get.isDialogOpen == true) {
+        if (dialogKey.currentContext != null) {
+          Navigator.of(dialogKey.currentContext!).pop();
+        } else {
           Get.back();
         }
-        Get.back();
+
+        // dialogKey.currentState?.pop();
+        // Navigator.pop()
       }
     });
     super.onInit();
